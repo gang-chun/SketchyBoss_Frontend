9a08c861c92a81c35e4f826ab2953fd6e1436c3b
Is2lucy1 <lchung@unomaha.edu>
2021-12-02 19:58:42 -0700

    Update Register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -11,7 +11,7 @@
 
 
                  <v-text-field
-                      v-model="userData.email"
+                      v-model="userData.account_emailaddress"
                       label="email"
                       required
                       type="email"
